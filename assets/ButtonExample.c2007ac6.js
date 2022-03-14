7e3cdd564a20038839fb2e60701baaa8bf33aa0e
aymerickmichelet <aymerickmichelet@users.noreply.github.com>
2022-03-14 17:55:03 +0000

    deploy: a8357bd15e687aceddff8d99b690aff6199a56e2

--- assets/ButtonExample.c2007ac6.js
+++ assets/ButtonExample.c2007ac6.js
@@ -1,4 +1,4 @@
-import{_ as u}from"./TwCodePreviewEditor.2d685a96.js";import{z as d,e as i,j as s,i as t,B as o,o as r}from"./vendor.d121aa1e.js";import{T as a}from"./TwButton.72b40949.js";import{_ as c}from"./index.80f6b431.js";import"./TwIcon.5b113f66.js";import"./index.c3c05765.js";var b=`<div>
+import{_ as u}from"./TwCodePreviewEditor.d98606e4.js";import{z as d,e as i,j as s,i as t,B as o,o as r}from"./vendor.d121aa1e.js";import{T as a}from"./TwButton.1ccd5694.js";import{_ as c}from"./index.9d20937b.js";import"./TwIcon.d2af77e1.js";import"./index.c3c05765.js";var b=`<div>
   <tw-button>Default</tw-button>
   <tw-button type="primary">Primary</tw-button>
   <tw-button type="success">Success</tw-button>
